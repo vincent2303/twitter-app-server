4fcea995fe91020868b33d82fa672191ad8f4338
Vincent Martin <vincent.martin23@gmail.com>
2018-08-26 16:15:07 +0200

    functionnality pause stream

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+let Twitter = require('twitter');
+
+
 app.get('/', function(req, res){
   res.send('<h1>server running</h1>');
 });
@@ -14,59 +17,57 @@ http.listen(process.env.port, function(){
   console.log('listening on *:' + process.env.port);
 });
 
-
 // try to get coord of tweet
 let getCoord = function(event){
   let coord = "no coord" 
   if (event.coordinates || event.place) {
     if (event.coordinates) {
-      coord = event.coordinates
+      return event.coordinates.coordinates
     }
     else if (event.place) {
-      coord = event.place.bounding_box.coordinates[0][0]
-      coord = [coord[1], coord[0]]
+      return event.place.bounding_box.coordinates[0][0]
     }
   }
   return coord
 }
 
+let nombre = 0;
 // send the tweet
 let sendTweet = function(io, event){
+  nombre++
   let coord = getCoord(event)
   let id_str= event.id_str
-  io.emit("tweet", {coord: coord, id: id_str})
+  if (nombre%5==0) {
+    console.log(nombre)
+  }
+  if (event.user) {
+    io.emit("tweet", {coord: coord, id: id_str, name: event.user.name, followers: event.user.followers_count})
+  }
+  else{
+    io.emit("tweet", {coord: coord, id: id_str})
+  }
 }
-
-
-// connect twitterStream
-let tweetStream = function(io, searchTerm){
-  console.log("tweet stream active")
-  let Twitter = require('twitter');
-
-  let client = new Twitter({
-      consumer_key: process.env.TWITTER_CONSUMER_KEY,
-      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-      access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-  });
-  var stream = client.stream('statuses/filter', {track: searchTerm});
-  stream.on('data', function(event) {
-    sendTweet(io, event)
-  });
-  stream.on('error', function(error) {
-    throw error;
-  });
-}
-
-let a = 3333333;
 
 io.on('connection', function(socket){
-  console.log("a user connect")
-  socket.on("pauseRequest", ()=>{
-    console.log("pause res")
-  })
+  console.log("user connection")
+  let client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
   socket.on("streamRequest", (searchWord)=>{
-    console.log("stream res:", searchWord)
-    tweetStream(io, searchWord)
+    console.log("stream request")
+    let activeStream;
+    activeStream = client.stream('statuses/filter', {track: searchWord});
+    activeStream.on('data', function(event) {
+      sendTweet(io, event)
+    });
+    socket.on("pauseRequest", ()=>{
+      activeStream.destroy()
+    })
+    activeStream.on('error', function(error) {
+      throw error;
+    });
   })
 });
